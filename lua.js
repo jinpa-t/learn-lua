4090822d6c9a6c9ff0b9a9246c86e1b8476d66fd
J Tsering <jacklaama@icloud.com>
2022-01-18 12:10:38 -0500

    new version

--- lua.js
+++ lua.js
@@ -1,6 +1,6 @@
 /**
  * @license
- * Copyright (C) 2008 Google Inc.
+ * Copyright (C) 2022 JT.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
